bc54eeb86972fff6f01b7ae3b43c5477090e5be8
Daniel Pino <dannyp32@live.com>
2015-01-29 22:44:43 -0800

    Added 5 day weather forecast using forecast.io api and geolocation.

---
+++ js/weather.js
@@ -0,0 +1,68 @@
+(function($) {
+    var getUserLoc = function() {
+        navigator.geolocation.getCurrentPosition(
+            function(geo) { 
+                if (geo) {
+                    getWeather(geo);
+                }
+            });
+    }
+    
+    var getWeather = function(geo) {
+        $.ajax({
+            url:"https://api.forecast.io/forecast/f6bf5ad03324ee03295b1ad3ad9feff1/" + geo.coords.latitude + "," + geo.coords.longitude,
+            dataType: 'jsonp', // Notice! JSONP <-- P (lowercase)
+            success: function (weatherData) {
+                console.log(weatherData);
+                displayWeather(weatherData);
+            },
+            error:function(){
+                alert("Error");
+            }     
+        });
+    }
+    
+    var displayWeather = function(weatherData) {
+        if (weatherData) {
+            dailyWeather = weatherData.daily.data;
+debugger;
+                            $("#day0").css("background", 'url("../images/' + dailyWeather[0].icon + '.png"' );
+            for (var i = 0; i < 5; i++) {
+                $("#day" + i).html(
+                    '<img class="weather-icon" src="../images/' + dailyWeather[i].icon + '.png"></img>' + 
+                    '<div class="degrees">' + (dailyWeather[i].apparentTemperatureMax | 0) + '<span class="degrees-symbol">°</span></div>' + 
+                    '<div class="weather-description">' + getWeatherDescription(dailyWeather[i].icon) + '</div>'
+                );         
+            }
+        }
+    }
+    
+    var getWeatherDescription = function (iconName) {
+        switch (iconName) {
+                case "clear-day": 
+                    return "Sunny";
+                case "clear-night": 
+                    return "Clear";
+                case "Cloudy": 
+                    return "Cloudy";
+                case "fog":
+                    return "Foggy";
+                case "partly-cloudy-day":
+                    return "Partly Cloudy";
+                case "partly-cloudy-night":
+                    return "Partly Cloudy";
+                case "rain":
+                    return "Rain";
+                case "sleet":
+                    return "Sleet";
+                case "snow":
+                    return "Snow";
+                case "wind":
+                    return "Windy";
+                default:
+                    return "Clear";
+        }
+    }
+    
+    getUserLoc();
+})(jQuery);
